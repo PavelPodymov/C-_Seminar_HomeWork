13f90d07343456e5beb1ea504d97e1a9b626f09a
PavelPodymov <poll.ppbpower@gmail.com>
2022-10-12 11:02:32 +0300

    Done Ex 54 Home

---
+++ SemHome_8.1/EX54_Home/Program.cs
@@ -0,0 +1,104 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу,
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int checkNumber(string UserString, bool OnlyMarkPlus)
+{
+    int numb = new int();
+    bool startCount = true;
+    while (startCount)
+    {
+        Console.Write(UserString);
+        var user_number = Console.ReadLine();
+
+        int.TryParse(user_number, out numb);
+
+        if (numb <= 0 && OnlyMarkPlus == true) startCount = true;
+        else
+        {
+            if (numb == 0 && user_number != "0") startCount = true;
+            else startCount = false;
+        }
+    }
+    return numb;
+}
+
+void FillMatrix(int[,] matrix, int start, int end)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int column = 0; column < matrix.GetLength(1); column++)
+        {
+            matrix[row, column] = new Random().Next(start, end + 1);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    Console.WriteLine($"rows = {matrix.GetLength(0)}, columns = {matrix.GetLength(1)}");
+
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int column = 0; column < matrix.GetLength(1); column++)
+        {
+            Console.Write($"{matrix[row, column]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortOfMatrixFromMinToMax(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            for (int column = 0; column < matrix.GetLength(1) - 1; column++)
+            {
+                if (matrix[row, column] <= matrix[row, column + 1])
+                {
+                    int buffer = matrix[row, column];
+                    matrix[row, column] = matrix[row, column + 1];
+                    matrix[row, column + 1] = buffer;
+                }
+            }
+        }
+    }
+}
+
+
+Console.Clear();
+int NumberOfRows = checkNumber("Please input a number of rows: ", true);
+int NumberOfColumns = checkNumber("Please input a number of columns: ", true);
+
+int startDiapason = checkNumber("Start diapason: ", false);
+int endDiapason = checkNumber("End diapason: ", false);
+
+int[,] MyMatrix = new int[NumberOfRows, NumberOfColumns];
+
+Console.WriteLine();
+Console.WriteLine("Start matrix: ");
+FillMatrix(MyMatrix, startDiapason, endDiapason);
+PrintMatrix(MyMatrix);
+Console.WriteLine();
+
+int[,] teacherMatrix = {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+SortOfMatrixFromMinToMax(MyMatrix);
+PrintMatrix(MyMatrix);
+SortOfMatrixFromMinToMax(teacherMatrix);
+PrintMatrix(teacherMatrix);
