6e660c8149024e8964efb0edae84616b44733876
PavelPodymov <poll.ppbpower@gmail.com>
2022-10-14 20:32:46 +0300

    Ex 67 Done

--- SemHome_9.1/Ex67_Sem/Program.cs
+++ SemHome_9.1/Ex67_Sem/Program.cs
@@ -1,5 +1,22 @@
 ﻿/*
-Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+Задача 67: Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
 453 -> 12
 45 -> 9
 */
+
+int SumNumericInNumbers(int number)
+{
+    if (number == 0) return 0;
+    else return number % 10 + SumNumericInNumbers(number / 10);
+}
+
+Console.Write("Input a number: ");
+int num;
+Int32.TryParse(Console.ReadLine(), out num);
+
+if (num > 0)
+{
+    Console.WriteLine($"{num} -> {SumNumericInNumbers(num)}");
+}
+else Console.WriteLine($"Sorry you write wrong number");
