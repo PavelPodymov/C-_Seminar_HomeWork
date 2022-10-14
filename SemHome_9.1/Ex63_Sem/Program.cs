5eb8e312530dd09199969ca1f5afae9067c4872e
PavelPodymov <poll.ppbpower@gmail.com>
2022-10-14 18:33:22 +0300

    Ex63 done

--- SemHome_9.1/Ex63_Sem/Program.cs
+++ SemHome_9.1/Ex63_Sem/Program.cs
@@ -4,3 +4,18 @@
 N = 5 -> "1, 2, 3, 4, 5"
 N = 6 -> "1, 2, 3, 4, 5, 6"
 */
+
+string NumberRange(int start, int end)
+{
+    if (start <= end) return $"{start}{((start == end)? "\"" : ',')} " + NumberRange(start + 1, end);
+    else return String.Empty;
+}
+
+Console.Write("Input a number: ");
+int n;
+if (int.TryParse(Console.ReadLine(), out n) && n > 0)
+{
+    Console.WriteLine($"N = {n} -> \"{NumberRange(1, n)}");
+}
+else Console.WriteLine($"Sorry you write wrong number");
+
