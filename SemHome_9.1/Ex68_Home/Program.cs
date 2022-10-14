449a6b89b5bc54623002849d2eb94b6436485983
PavelPodymov <poll.ppbpower@gmail.com>
2022-10-14 22:09:18 +0300

    Akkerman

--- SemHome_9.1/Ex68_Home/Program.cs
+++ SemHome_9.1/Ex68_Home/Program.cs
@@ -7,3 +7,23 @@ m = 3, n = 2 -> A(m,n) = 29
 
 */
 
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(m, n);
+}
+
+Console.Write("Input m: ");
+int m;
+Int32.TryParse(Console.ReadLine(), out m);
+Console.Write("Input n: ");
+int n;
+Int32.TryParse(Console.ReadLine(), out n);
+
+if (m >= 0 && n >= 0)
+{
+    Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {Akkerman(m, n)}");
+}
+else Console.WriteLine($"Sorry you write wrong number");
