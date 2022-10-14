8da5f97e40ff82ed9e32c751361bf2ccff4f9070
PavelPodymov <poll.ppbpower@gmail.com>
2022-10-14 19:22:02 +0300

    Ex 66 done

--- SemHome_9.1/Ex66_Home/Program.cs
+++ SemHome_9.1/Ex66_Home/Program.cs
@@ -6,3 +6,23 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
 
+int SumNumbers(int start, int end )
+{
+    if (start > end) return 0;
+    else return end + SumNumbers(start, end - 1);
+}
+
+
+Console.Write("Input a number start: ");
+int start;
+Int32.TryParse(Console.ReadLine(), out start);
+
+Console.Write("Input a number End: ");
+int end;
+Int32.TryParse(Console.ReadLine(), out end);
+
+if (end > 0 && start > 0)
+{
+    Console.WriteLine($"M = {start}; N = {end} -> {SumNumbers(start, end)}");
+}
+else Console.WriteLine($"Sorry you write wrong number");
