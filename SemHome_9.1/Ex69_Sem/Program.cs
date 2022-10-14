48b89496b95c52a951dc742e312cf939dd46744b
PavelPodymov <poll.ppbpower@gmail.com>
2022-10-14 20:48:10 +0300

    Ex 69 done

--- SemHome_9.1/Ex69_Sem/Program.cs
+++ SemHome_9.1/Ex69_Sem/Program.cs
@@ -4,3 +4,24 @@
 A = 3; B = 5 -> 243 (3⁵)
 A = 2; B = 3 -> 8
 */
+
+int Power(int a, int b)
+{   
+    return b == 0 ? 1 : a * Power(a, b - 1);
+}
+
+
+Console.WriteLine(Power(2, 3));
+
+Console.Write("Input a: ");
+int a;
+Int32.TryParse(Console.ReadLine(), out a);
+Console.Write("Input b: ");
+int b;
+Int32.TryParse(Console.ReadLine(), out b);
+
+if (a >= 0 && b >=0)
+{
+    Console.WriteLine($"A = {a}; B = {b} -> {Power(a, b)}");
+}
+else Console.WriteLine($"Sorry you write wrong number");
