9b3d43cfe4f366d896cc2f670e07c5adb78b2c99
PavelPodymov <poll.ppbpower@gmail.com>
2022-10-14 19:09:00 +0300

    Ex 65 done

--- SemHome_9.1/Ex65_Sem/Program.cs
+++ SemHome_9.1/Ex65_Sem/Program.cs
@@ -4,3 +4,23 @@
 M = 1; N = 5 -> "1, 2, 3, 4, 5"
 M = 4; N = 8 -> "4, 6, 7, 8"
 */
+
+string NumberRange(int start, int end)
+{
+    if (start <= end) return $"{start}{((start == end)? "\"" : ',')} " + NumberRange(start + 1, end);
+    else return String.Empty;
+}
+
+Console.Write("Input a number start: ");
+int start;
+Int32.TryParse(Console.ReadLine(), out start);
+
+Console.Write("Input a number End: ");
+int end;
+Int32.TryParse(Console.ReadLine(), out end);
+
+if (end > 0 && start > 0)
+{
+    Console.WriteLine($"M = {start}; N = {end} -> \"{NumberRange(start, end)}");
+}
+else Console.WriteLine($"Sorry you write wrong number");
