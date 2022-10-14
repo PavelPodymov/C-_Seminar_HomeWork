13730ca2d111f11ce84986d36bbf4eed3b29be7c
PavelPodymov <poll.ppbpower@gmail.com>
2022-10-14 18:55:57 +0300

    Ex 64 Done

--- SemHome_9.1/Ex64_Home/Program.cs
+++ SemHome_9.1/Ex64_Home/Program.cs
@@ -6,3 +6,17 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+
+string NumberRange(int start, int end)
+{
+    if (start >= end) return $"{start}{((start == end)? "\"" : ',')} " + NumberRange(start - 1, end);
+    else return String.Empty;
+}
+
+Console.Write("Input a number: ");
+int n;
+if (int.TryParse(Console.ReadLine(), out n) && n > 0)
+{
+    Console.WriteLine($"N = {n} -> \"{NumberRange(n, 1)}");
+}
+else Console.WriteLine($"Sorry you write wrong number");
